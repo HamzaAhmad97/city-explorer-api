da7ee6c0ae904703237f53dbdbe66a57b7f309e2
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-08-14 20:28:38 +0300

    finally

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 //const weatherData = require('./data/weather.json');
-const cors = require('cors');
+const CORS = require('cors');
 const app = express();
-app.use(cors());
+app.use(CORS());
+
 require('dotenv').config();
 const PORT = process.env.PORT;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
